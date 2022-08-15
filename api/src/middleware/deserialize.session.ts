b937a9b40b3a94794a49319eb22334a9e7b563e2
adeelkbw <adeelnasirkbw@gmail.com>
2022-08-16 03:46:08 +0500

    sk

--- api/src/middleware/deserialize.session.ts
+++ api/src/middleware/deserialize.session.ts
@@ -1,23 +1,23 @@
 import { Response, Request, NextFunction } from 'express'
-import { verifyToken } from '../jwt.utils'
+import { jwtSign, verifyToken } from '../jwt.utils'
 
 export async function deserialize(
   req: Request,
   res: Response,
   next: NextFunction
 ) {
-  const { accessToken } = req.cookies
+  const { accessToken, refreshToken } = req.cookies
 
   if (!accessToken) return next()
 
-  const { decoded } = await verifyToken(accessToken)
-  console.log('This', decoded)
-
+  //valid token
+  const { decoded, expired } = await verifyToken(accessToken)
   if (decoded) {
     //@ts-ignore
     req.user = decoded
 
     return next()
   }
+
   next()
 }
