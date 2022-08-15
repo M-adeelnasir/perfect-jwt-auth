15cb0ab8ce18b197a450b1bcd79a96139286739d
adeelkbw <adeelnasirkbw@gmail.com>
2022-08-16 00:25:59 +0500

    in db opratoion --db

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,10 +1,12 @@
 import express from "express";
 import cors from 'cors'
-
+import cookieParser from 'cookie-parser'
 
 const app = express()
 
 app.use(cors())
+app.use(cookieParser())
+
 
 const server = app.listen(9100, () => {
     console.log("Server is up and running http://localhost:9100");
